becf07e08c621c5f214a97ccd62f8e5c1fb48ea5
jamie <jamiedotpro@gmail.com>
2019-07-29 16:51:01 +0900

    backup

--- keras/keras22_mnist_cnn1.py
+++ keras/keras22_mnist_cnn1.py
@@ -21,9 +21,20 @@ import tensorflow as tf
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32') / 255
 X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32') / 255
+print(Y_train.shape)    # (60000, )
+print(Y_test.shape)
 Y_train = np_utils.to_categorical(Y_train)
 Y_test = np_utils.to_categorical(Y_test)
+print(Y_train.shape)    # (60000, 10) 원-핫 인코딩(One-hot encoding)...
+print(Y_test.shape)     # 10은 10개로 데이터가 분류된다는 거
 
+# 0부터 9까지의...
+# print(X_train.shape)
+# print(X_test.shape)
+# print(Y_train.shape)
+# print(Y_test.shape)
+
+'''
 # 컨볼루션 신경망의 설정
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3,3), input_shape=(28,28,1), activation='relu'))
@@ -33,8 +44,9 @@ model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
+model.add(Dense(10, activation='softmax'))  # 분류 모델을 사용하려면 마지막에 무조건 activation='softmax'로 설정해야함
 
+# loss='categorical_crossentropy': 분류 모델에서는 이거로 써야 함
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # 모델 최적화 설정
@@ -54,4 +66,4 @@ history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test),
 
 # 테스트 정확도 출력
 print('\n Test Accuracy: %.4f' % (model.evaluate(X_test, Y_test)[1]))
-    
+'''
