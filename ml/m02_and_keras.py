0e47cd506ac4e5993b59ace3e7b15c1fb0c9bdbb
jamie <jamiedotpro@gmail.com>
2019-08-05 11:09:45 +0900

    backup

---
+++ ml/m02_and_keras.py
@@ -0,0 +1,36 @@
+# 선형회귀
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, BatchNormalization, Dropout
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+
+# 1. 데이터
+x_data = np.array([[0,0], [1,0], [0,1], [1,1]])
+y_data = np.array([0,0,0,1])  # 위 데이터 and한 결과
+
+
+# 2. 모델
+model = Sequential()
+model.add(Dense(8, input_shape=(2, ), activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.summary()
+
+
+# 3. 실행
+model.compile(loss='mse', optimizer='adam', metrics=['acc'])
+model.fit(x_data, y_data, epochs=2000, batch_size=1, verbose=1)
+
+
+# 4. 평가 예측
+x_test = np.array([[0,0], [1,0], [0,1], [1,1]])
+y_test = np.array([0,0,0,1])
+loss, acc = model.evaluate(x_test, y_test)
+
+y_predict = np.round(model.predict(x_test))
+print('loss: ', loss)
+print('acc: ', acc)
+print(y_predict)
+# import sys
+# sys.exit()
