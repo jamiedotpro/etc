5d3d43a8ed119db44cc75e92d1c5757cba9e4e47
jamie <jamiedotpro@gmail.com>
2019-08-13 18:26:08 +0900

    backup

--- ml/m31_vgg.py
+++ ml/m31_vgg.py
@@ -15,3 +15,11 @@ model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.summary()
+
+# 다운로드
+# from keras.applications import Xception, InceptionV3, ResNet50, VGG19, MobileNet
+# conv_base = Xception()
+# conv_base = InceptionV3()
+# conv_base = ResNet50()
+# conv_base = VGG19()
+# conv_base = MobileNet()
