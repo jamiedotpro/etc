a71da6412df6dbc5673fc73763b48a3e8a3de397
jamiedotpro <jamiedotpro@gmail.com>
2019-08-23 19:19:19 +0900

    backup

--- tf/tf18_cnn.py
+++ tf/tf18_cnn.py
@@ -16,14 +16,19 @@ y = tf.placeholder(tf.float32, [None, 10])
 
 # L1 ImgIn shape={?, 28, 28, 1}
 w1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))  # [커널사이즈,, 흑백/컬러, 아웃풋]
-print('w1: ', w1)
+print('w1: ', w1)   # w1:  <tf.Variable 'Variable:0' shape=(3, 3, 1, 32) dtype=float32_ref>
 #   Conv    -> (?, 28, 28, 32)
 #   Pool    -> (?, 14, 14, 32)
 l1 = tf.nn.conv2d(x_img, w1, strides=[1, 1, 1, 1], padding='SAME')  # strides 몇칸씩 자를 것인가. 가로 세로 동일하게. strides=[안씀, 가로, 세로, 안씀]
-print('l1: ', l1)
+print('l1: ', l1)   # l1:  Tensor("Conv2D:0", shape=(?, 28, 28, 32), dtype=float32)
 l1 = tf.nn.relu(l1)
-l1 = tf.nn.max_pool(l1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')   # 맥스풀링은 겹쳐지지 않은 상태로 지정범위내 높은 특성값을 출력한다. 반으로 크기가 줄어듬
-print('l1: ', l1)
+l1 = tf.nn.max_pool(l1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+# tf.nn.max_pool
+    # 맥스풀링은 겹쳐지지 않은 상태로 지정범위 내 높은 특성값을 출력한다. ksize처럼 계산되어 크기가 줄어듬
+    # ksize: [1, 2, 2, 1]은 2*2 범위에서 1개의 결과를 출력. 4개의 데이터 중 가장 큰 1개를 반환한다.
+    # strides:
+    # padding: 크기가 홀수인 경우 나누어 떨어지지 않기 때문에 필요. 이전 값이 홀수라면 +1된 상태에서 max_pool이 처리된다.
+print('l1: ', l1)   # l1:  Tensor("MaxPool:0", shape=(?, 14, 14, 32), dtype=float32)
 
 # L2 ImgIn shape=(?, 14, 14, 32)
 w2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01)) # [커널사이즈:몇by몇으로 자를지,, 이전 아웃풋, 아웃풋]
