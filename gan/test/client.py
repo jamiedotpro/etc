54c88ef982ac8b02ce23903cc8547b4da3409ff6
jamiedotpro <jamiedotpro@gmail.com>
2019-10-06 12:03:46 +0900

    backup

--- gan/test/client.py
+++ gan/test/client.py
@@ -1,10 +1,9 @@
 import socket
 import time
 HOST ='192.168.0.159'
-# HOST ='imagenius.iptime.org' 
-
+# HOST ='BitAi.iptime.org'
 PORT = 9009
-PORT2 = 9010
+PORT2 = 9010 #gen
  
 def getFileFromServer(filename):
     data_transferred = 0
@@ -42,7 +41,7 @@ def postFiletoServer(filename):
         #     print("같은 이름의 데이터가 존재합니다.")
         #     return
         # print(1)
-        time.sleep(1)
+        
         with open('./upload/' + filename,'rb') as f:           
             try:
                 data = f.read(1024)
@@ -50,9 +49,6 @@ def postFiletoServer(filename):
                     sock.send(data)
                     data_transferred += len(data)
                     data = f.read(1024)
-
-
-
             # l = f.read(1024)
             # while (l):
             #     print('Sending...')
@@ -62,12 +58,27 @@ def postFiletoServer(filename):
             # print("Done Sending")
             except Exception as e:
                 print(e)
-    print('파일[%s] 전송종료. 전송량 [%d]' %(filename, data_transferred))
+        time.sleep(1)
+    #     data = sock.recv(1024)
+    #     if not data:
+    #         print('파일[%s]: 서버에 존재하지 않거나 전송중 오류발생' %filename)
+    #         return
 
-clientMode = int(input("모드를 선택하세요: 1. 파일다운로드, 2.파일업로드"))
-if clientMode == 1:
-    filename = input('다운로드 받은 파일이름을 입력하세요:')
-    getFileFromServer(filename)
-elif clientMode == 2:
-    filename = input('업로드할 파일이름을 입력하세요:')
-    postFiletoServer(filename)
+    #     with open('./gen_img/' + filename,'wb') as f:
+    #         try:
+    #             while  data:
+    #                 f.write(data)
+    #                 data_transferred += len(data)
+    #                 data = sock.recv(1024)
+    #         except Exception as e:
+    #             print(e)
+
+    # print('파일[%s] 전송종료. 전송량 [%d]' %(filename, data_transferred))
+while 1:
+    clientMode = int(input("모드를 선택하세요: 1. 파일다운로드, 2.파일업로드"))
+    if clientMode == 1:
+        filename = input('다운로드 받은 파일이름을 입력하세요:')
+        getFileFromServer(filename)
+    elif clientMode == 2:
+        filename = input('업로드할 파일이름을 입력하세요:')
+        postFiletoServer(filename)
--- gan/test/server.py
+++ gan/test/server.py
@@ -1,7 +1,9 @@
 import socketserver, threading
 from os.path import exists
+import os
 import socket
-
+import generate as gen
+path = os.getcwd()
 HOST ='192.168.0.159'
 PORT = 9009 # 다운로드 서버 -> 클라이언트
 PORT2 = 9010 # 업로드 클라이언트 -> 서버
@@ -46,8 +48,7 @@ class MyTcpHandler(socketserver.StreamRequestHandler):
         print('전송완료[%s], 전송량[%d]' %(filename,data_transferred))
 
 class MyTcpUpload(socketserver.BaseRequestHandler):
-    timeout = 3
-    # self.request.settimeout(1)
+    
     def handle(self):
         
         # self.request.settimeout(1)
@@ -78,7 +79,54 @@ class MyTcpUpload(socketserver.BaseRequestHandler):
             except Exception as e:
                 print(e)
         print('파일[%s] 전송종료. 전송량 [%d]' %(filename, data_transferred))
+        
 
+
+class MyTcpHandler_gen(socketserver.StreamRequestHandler):
+    def handle(self):
+        
+        # self.request.settimeout(1)
+
+        data_transferred = 0
+        print('[%s] 연결됨' %self.client_address[0])
+        sock = self.request
+
+        filename = self.request.recv(1024) # 클라이언트로 부터 파일이름을 전달받음
+        filename = filename.decode()
+        print(filename)
+        # if exists(filename): # 파일이 해당 디렉터리에 존재하면
+        #     self.request.send("1010")
+        #     return # handle()함수를 빠져 나온다.
+        # print(1)
+        data = self.request.recv(1024) # 클라이언트로 부터 이미지를 전달받음
+        if not data:
+            print('파일[%s]: 서버에 존재하지 않거나 전송중 오류발생' %filename)
+            return
+
+        with open(path + '/upload/' + filename,'wb') as f:
+            try:
+                while  data:
+                    f.write(data)
+                    data_transferred += len(data)
+                    data = sock.recv(1024)
+                    
+            except Exception as e:
+                print(e)
+        print('파일[%s] 전송종료. 전송량 [%d]' %(filename, data_transferred))
+
+        gen.gen(filename)# gen
+
+        print('파일[%s] 전송 시작...' %filename)
+        with open(filename,'rb') as f:
+            try:
+                data = f.read(1024) # 파일을 1024바이트 읽음
+                while data: # 파일이 빈 문자열일때까지 반복
+                    data_transferred += self.request.send(data)
+                    data = f.read(1024)
+            except Exception as e:
+                print(e)
+ 
+        print('전송완료[%s], 전송량[%d]' %(filename,data_transferred))
 
 
 import select
@@ -86,7 +134,8 @@ def runServer():
     print('++++++파일 서버를 시작++++++')
     print("+++파일 서버를 끝내려면 'Ctrl + C'를 누르세요.")
     server1 = socketserver.TCPServer(ADDR1,MyTcpHandler)
-    server2 = socketserver.TCPServer(ADDR2,MyTcpUpload)
+    # server2 = socketserver.TCPServer(ADDR2,MyTcpUpload)
+    server2 = socketserver.TCPServer(ADDR2,MyTcpHandler_gen)
     ip1, port1 = server1.server_address
     ip2, port2 = server2.server_address
     read_port_list =[server1,server2]
@@ -97,36 +146,31 @@ def runServer():
         # print(conn_read_socket_list)
         for conn_read_socket in conn_read_socket_list:
             if conn_read_socket == server1:
-                print("MODE DOWNLOAD")
-                server_thread = threading.Thread(target=server1.serve_forever)
-                server_thread.daemon = True
-                server_thread.start()
+                try:
+                    print('MODE : DOWNLOAD')
+                    server_thread = threading.Thread(target=server1.serve_forever)
+                    server_thread.daemon = True
+                    server_thread.start()
+                    
+                except KeyboardInterrupt:
+                    print('++++++파일 서버를 종료합니다.++++++')
+                
+                
             
             elif conn_read_socket == server2:
                 print("MODE UPLOAD")
-                # server2.handle_request()
-                server_thread = threading.Thread(target=server2.serve_forever)
-                server_thread.daemon = True
-                server_thread.start()
+                try:
+                    print('MODE : UPLOAD')       
+                    server_thread = threading.Thread(target=server2.serve_forever)
+                    server_thread.daemon = True
+                    server_thread.start()
+                
+                except KeyboardInterrupt:
+                    print('++++++파일 서버를 종료합니다.++++++')
+                    
+                
         
-        # try:
-        #     print('MODE : DOWNLOAD')
-            
-            
-        #     # server.handle_request()
-            
-        # except KeyboardInterrupt:
-        #     print('++++++파일 서버를 종료합니다.++++++')
-
-        # try:
-        #     print('MODE : UPLOAD')
-            
-            
-
-        #     # server.serve_forever()
-        
-        # except KeyboardInterrupt:
-        #     print('++++++파일 서버를 종료합니다.++++++')
+    
         
  
  
